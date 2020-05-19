f66921f309f8555fe6aadcb8d168037b08db47a1
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-19 16:13:04 +0900

    초기 커밋.

---
+++ mybook/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mybook
+{
+    class Book
+    {
+        public string name;
+        public string firstRelease;
+        public string writer;
+        public string publisher;
+        public string publisherCenter;
+        public string editerChief;
+        public string planner;
+        public string editer;
+        public string design;
+    }
+}
---
+++ mybook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mybook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.name = "PHP 프로그래밍 입문";
+            book.firstRelease = "2013년 5월 20일";
+            book.writer = "황재호";
+            book.publisher = "김태현";
+            book.publisherCenter = "한빛아카데미(주)";
+            book.editerChief = "김현용";
+            book.planner = "김이화";
+            book.editer = "김이화";
+            book.design = "여동일";
+
+
+
+        }
+    }
+}
